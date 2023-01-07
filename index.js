712bf2dec56b5b662d21de1269978921bd952352
ohizz <jtsamuel3084@gmail.com>
2023-01-07 16:13:38 +0100

    no index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8080;
 
 // app.use(cors());
 app.use(express.json());
-app.use('/sneakers', route);
+app.use('/', route);
 app.use(bodyparser.json());
 
 database.on('error', (error) => {
