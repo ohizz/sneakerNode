ff2f49cb5c72dc1a28c95294464bc7b4af82a11a
ohizz <jtsamuel3084@gmail.com>
2023-01-07 16:28:19 +0100

    vwhat

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
-app.use('/', route);
+app.use('/sneakers', route);
 app.use(bodyparser.json());
 
 database.on('error', (error) => {
